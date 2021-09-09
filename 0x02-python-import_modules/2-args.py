4f517883626b7382da21c1a35284347be29fe9a7
Toby-Codes <twiseas9ice@gmail.com>
2021-09-09 13:50:43 -0700

    [Add] Solution to task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    if len(argv) == 1:
-        print("{:d} arguments.".format(len(argv) - 1))
-    elif len(argv) == 2:
-        print("{:d} argument:".format(len(argv) - 1))
-        for a in range(1, len(argv)):
-            print("{:d}: {}".format(a, argv[a]))
+    import sys
+    num = len(sys.argv)
+    if num == 1:
+        print("{} arguments.".format(num - 1))
+    elif num == 2:
+        print("{} argument:".format(num - 1))
     else:
-        print("{:d} arguments:".format(len(argv) - 1))
-        for a in range(1, len(argv)):
-            print("{:d}: {}".format(a, argv[a]))
+        print("{} arguments:".format(num - 1))
+
+    for i in range(1, num):
+        print("{}: {}".format(i, sys.argv[i]))
