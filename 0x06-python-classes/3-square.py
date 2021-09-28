ddc0b0cc986974b423903937c09b59974f55e1f3
Toby-Codes <twiseas9ice@gmail.com>
2021-09-28 13:02:15 -0700

    [Add] Solution to task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,6 +3,7 @@ class Square:
     """Represents a square.
     Private instance attribute: size.
     Instantiation with optional size.
+    Public instance method: def area(self).
     """
 
     def __init__(self, size=0):
@@ -12,3 +13,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
