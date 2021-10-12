41d38e17669c1039104ca06afcea98090d384f63
Toby-Codes <twiseas9ice@gmail.com>
2021-10-12 15:22:31 -0700

    <fix> errors

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,21 +6,15 @@ and then save them to a file.
 
 
 import sys
-import json
-import os.path
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+arglist = list(sys.argv[1:])
 
-my_file = 'add_item.json'
+try:
+    old_data = load_from_json_file('add_item.json')
+except Exception:
+    old_data = []
 
-my_list = []
-
-if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
-    my_list = load_from_json_file(my_file)
-
-if len(sys.argv) > 1:
-    for elem in sys.argv[1:]:
-        my_list.append(elem)
-
-save_to_json_file(my_list, my_file)
+old_data.extend(arglist)
+save_to_json_file(old_data, 'add_item.json')
