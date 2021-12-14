26e52acd2b566fb152c33e1b8e1ffc7014a9ff1e
Paul Tobiloba <48536008+Toby-Codes@users.noreply.github.com>
2021-12-14 22:52:36 +0100

    Update 1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-/*
-    define rectangle class with properties
-    height and width
-*/
 
 class Rectangle {
-	constructor (w, h) {
-		this.width = w;
-		this.height = h;
-	}
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 }
+
 module.exports = Rectangle;
